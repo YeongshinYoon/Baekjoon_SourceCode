f351cea1193d2fd8729ef58c1792e9a5b8b50916
YeongshinYoon <44855654+YeongshinYoon@users.noreply.github.com>
2020-04-17 18:14:11 +0900

    문제_단계별로 풀어보기_1단계_입출력과 사칙연산
    문제_단계별로 풀어보기_1단계_입출력과 사칙연산 소스코드 업로드

--- 문제/단계별로 풀어보기/1_입출력과 사칙연산/3_10171.c
+++ 문제/단계별로 풀어보기/1_입출력과 사칙연산/3_10171.c
@@ -1,3 +1,9 @@
+// 문제번호		10171
+// 문제이름		고양이
+// 문제분류		문제/단계별로 풀어보기/1_입출력과 사칙연산
+// 문제출처		acmicpc.net
+// 코드작성자	YOON Yeongshin
+
 #include <stdio.h>
 
 int main()
--- 문제/단계별로 풀어보기/1_입출력과 사칙연산/7_10998.c
+++ 문제/단계별로 풀어보기/1_입출력과 사칙연산/7_10998.c
@@ -1,3 +1,9 @@
+// 문제번호		10998
+// 문제이름		A×B
+// 문제분류		문제/단계별로 풀어보기/1_입출력과 사칙연산
+// 문제출처		acmicpc.net
+// 코드작성자	YOON Yeongshin
+
 #include <stdio.h>
 
 int main()
